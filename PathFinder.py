c5be1ba2465bc9200c674273396422a25af7cf5b
Pradyumn Vikram <54140659+PradyumnVikram@users.noreply.github.com>
2020-06-06 01:03:58 +0530

    Update PathFinder.py

--- PathFinder.py
+++ PathFinder.py
@@ -2,8 +2,6 @@ class Solver:
     def __init__(self, n_size, board):
         self.n_size = n_size
         self.board = board
-        self.sol_board = [[0 for n in range(self.n_size)] for row in range(self.n_size)]
-
 
     def print_board(self, board):
         for i in board:
@@ -11,39 +9,34 @@ class Solver:
             for j in i:
                 print(j, end=' ')
 
-
     def solve_maze(self):
         count = 0
-        if not self.solve(self.board, 0, 0, count):
+        if not self.solve(self.board, 0, 0):
             return False
         return self.board
-
 
     def isValid(self, x, y):
         if x >= 0 and x < self.n_size and y >= 0 and y < self.n_size and self.board[x][y] == 1:
             return True
         return False
 
-
-    def solve(self,board, x, y, count):
-
-        count += 1
+    def solve(self, board, x, y):
         if x == self.n_size - 1 and y == self.n_size - 1 and self.board[x][y] == 1:
             self.board[x][y] = 2
-            print('[Server]: Total backtracks:', count)
             return True
 
-        if self.isValid( x, y):
+        if self.isValid(x, y):
             self.board[x][y] = 2
 
-            if self.solve(self.board, x, y + 1, count):
+            if self.solve(self.board, x, y + 1):
                 return True
 
-            if self.solve(self.board, x + 1, y,  count):
+            if self.solve(self.board, x + 1, y):
                 return True
 
             self.board[x][y] = 0
             return False
 
 
-
+def new_board(n_size):
+    return [[1 for n in range(n_size)] for row in range(n_size)]
