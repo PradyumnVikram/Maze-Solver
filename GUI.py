9f0b2d6f74dacc9901af0e64b3f290944f5e6263
Pradyumn Vikram <54140659+PradyumnVikram@users.noreply.github.com>
2020-06-06 23:42:04 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -33,6 +33,12 @@ def redraw_window(screen, grid, solve):
                 color = WHITE
             elif grid[row][col] == 2:
                 color = BLUE
+            elif grid[row][col] == 4:
+                color = (0,220,255)
+            elif grid[row][col] == 3:
+                color = (0,220,255)
+            elif grid[row][col] == 5:
+                color = (0,255,0)
 
             pygame.draw.rect(win, color, [(MARGIN + WIDTH)*col + MARGIN,
                                           (MARGIN + HEIGHT)*row + MARGIN,
@@ -42,13 +48,21 @@ def redraw_window(screen, grid, solve):
     pygame.display.update()
 
 
-def get_col_row(maze):
+def get_col_row(maze, declared, start):
     pos = pygame.mouse.get_pos()
     col = pos[0]//(WIDTH+MARGIN)
     row = pos[1]//(HEIGHT+MARGIN)
     try:
-        if maze[row][col] == 1:
-            maze[row][col] = 0
+        if declared:
+            if maze[row][col] == 1:
+                maze[row][col] = 0
+        else:
+            if start % 2 != 0:
+                maze[row][col] = 3
+            elif start % 2 == 0:
+                maze[row][col] = 4
+        return [row, col]
+        
     except IndexError as e:
         pass
 
@@ -58,6 +72,8 @@ def main():
     clock = pygame.time.Clock()
     run = True
     solve = False
+    declared = False
+    start = 1
     while run:
         clock.tick(60)
         redraw_window(win, maze, solve)
@@ -71,25 +87,42 @@ def main():
                     run = False
                     pygame.quit()
             if not solve:
-                if pygame.mouse.get_pressed()[0]:
+                if pygame.mouse.get_pressed()[0] and declared:
                     try:
-                        get_col_row(maze)
+                        get_col_row(maze, declared, start)
                     except AttributeError:
                         pass
+                elif not declared:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if start % 2 != 0:
+                            start_pos = get_col_row(maze, declared, start)
+                            start += 1
+                        else:
+                            
+                            final = get_col_row(maze, declared, start)
+                            declared = True
 
             keys = pygame.key.get_pressed()
             if keys[pygame.K_RETURN]:
                 solve = True
                 engine = Solver(n_size, maze)
-
-                sol = engine.solve_maze()
+                maze[start_pos[0]][start_pos[1]] = 1
+                sol = engine.solve_maze(final, start_pos)
                 if sol:
                     maze = sol
+                    c = 0
+                    for x in maze:
+                        for val in x:
+                            if val == 5:
+                                c+=1
+                    print(c, ' cells visited before achieveing solution path')
                 else:
                     print('Not found')
                     solve = False
             elif keys[pygame.K_r]:
                 maze = PathFinder.new_board(n_size)
+                declared = False
+                start = 1
                 solve = False
     pygame.quit()
 
